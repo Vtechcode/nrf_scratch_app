7b973c9690f19c03cffeab6c34f3694c88d2c1db
Vtechcode <victorwaburi@gmail.com>
2025-04-01 07:48:07 +0300

    printk() app from scratch

---
+++ build/image_preload.cmake
@@ -0,0 +1,6 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf9160dk/nrf9160" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "C:/nordic/myApps/13_e2/nrf9160dk_nrf9160.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/2d382dcd92/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
